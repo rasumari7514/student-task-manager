138741917d1bf57e96c9ad5a8546f885f7c90b17
rasumari7514 <rasumari589@gmail.com>
2026-08-25 20:20:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ function addTask() {
   const input = document.getElementById("taskInput");
   const taskList = document.getElementById("taskList");
   const priority = document.getElementById("priority");
+  const dueDate = document.getElementById("dueDate");
 
   const task = input.value.trim();
   const selectedPriority = priority.value;
+  const selectedDate = dueDate.value;
 
   if (task === "") {
     alert("Please enter a task");
     return;
   }
 
+  if (selectedDate === "") {
+    alert("Please select a due date");
+    return;
+  }
+
   const li = document.createElement("li");
 
-  li.textContent = task + " - " + selectedPriority + " Priority ";
+  li.textContent =
+    task + " - " + selectedPriority + " Priority - Due: " + selectedDate + " ";
 
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
@@ -32,4 +40,5 @@ function addTask() {
   taskList.appendChild(li);
 
   input.value = "";
+  dueDate.value = "";
 }
