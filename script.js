934d124d58295969e4aa27c87ebb7a0f9fa7bd10
rasumari7514 <rasumari589@gmail.com>
2026-08-25 19:27:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function addTask() {
   const input = document.getElementById("taskInput");
-  const task = input.value;
+  const taskList = document.getElementById("taskList");
+
+  const task = input.value.trim();
 
   if (task === "") {
     alert("Please enter a task");
     return;
   }
 
-  alert("Task added: " + task);
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  taskList.appendChild(li);
+
   input.value = "";
 }
